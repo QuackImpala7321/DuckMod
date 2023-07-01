b6086c4a6c4a7edb43c569addc63ce261342545e
Rodrigo Martinez <you@example.com>
2023-06-30 23:33:04 -0500

    Initial commit

---
+++ src/client/java/net/quackimpala7321/duckmod/DuckModClient.java
@@ -0,0 +1,10 @@
+package net.quackimpala7321.duckmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DuckModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
---
+++ src/main/java/net/quackimpala7321/duckmod/DuckMod.java
@@ -0,0 +1,16 @@
+package net.quackimpala7321.duckmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DuckMod implements ModInitializer {
+    public static final String MOD_ID = "duckmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
